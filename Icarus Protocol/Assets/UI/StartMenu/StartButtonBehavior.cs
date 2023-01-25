43c79b54083d7e1c102ddcb092342e9cd2dedff8
aueti <anna.ueti@wsu.edu>
2023-01-24 19:09:23 -0800

    1. Added new inverted border and full while pngs, 2. Implemented custom onhover and offhover events for the start button

--- Icarus Protocol/Assets/UI/StartMenu/StartButtonBehavior.cs
+++ Icarus Protocol/Assets/UI/StartMenu/StartButtonBehavior.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
 
 /// <summary>
 /// The behavior for starting the game
 /// </summary>
 public class StartButtonBehavior : MonoBehaviour
 {
+
+    private TMP_Text text;
+    private FontWeight startingWeight;
+
     /// <summary>
     /// A reference to the UI Handler for facilitating layout transitions.
     /// </summary>
     public UILayoutHandler UiHandler;
+
+    private void Start()
+    {
+        text = GetComponentInChildren<TMP_Text>();
+        startingWeight = text.fontWeight;
+
+    }
 
     /// <summary>
     /// Called when the button is clicked.
@@ -18,5 +35,17 @@ public class StartButtonBehavior : MonoBehaviour
     public void OnClick()
     {
         UiHandler.TransitionToLevelSelect(0);
+    }
+
+    public void OnHover()
+    {
+        text.color = Color.black;
+        text.fontWeight = FontWeight.Bold;
+    }
+
+    public void OffHover() 
+    {
+        text.color = Color.white;
+        text.fontWeight = startingWeight;
     }
 }
