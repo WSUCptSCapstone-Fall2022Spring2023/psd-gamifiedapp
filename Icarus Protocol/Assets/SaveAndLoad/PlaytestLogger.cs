4cbf7dd374c0f025537adb7eec1e6e8a148c760c
Nelsocol <collinnelson509@gmail.com>
2023-02-26 16:35:21 -0800

    Added logging for 6 basic events we want to track during playtest

---
+++ Icarus Protocol/Assets/SaveAndLoad/PlaytestLogger.cs
@@ -0,0 +1,45 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public enum LogTypes { 
+    LEVEL_START,
+    LEVEL_EXIT,
+    PHASE_FAILURE,
+    PHASE_SUCCESS,
+    MANUAL_ACCESS,
+    MANUAL_EXIT
+}
+
+/// <summary>
+/// Handles the logs for the playtests recording the actions that players take in game for better hard data on play.
+/// </summary>
+public class PlaytestLogger : MonoBehaviour
+{
+    /// <summary>
+    /// Caches the path to the logfile
+    /// </summary>
+    private string logPath { get; set; }
+
+    /// <summary>
+    /// Awake runs when the object is loaded.
+    /// </summary>
+    void Awake()
+    {
+        string savePath = Application.persistentDataPath + $"/logs";
+        Directory.CreateDirectory(savePath);
+        logPath = savePath + $"/{DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")}-log.csv";
+    }
+
+    /// <summary>
+    /// Addes a line to the logfile
+    /// </summary>
+    public void CreateLog(LogTypes type, string context)
+    {
+        File.AppendAllText(logPath, $"{type}, {Time.realtimeSinceStartup}, {context}\n");
+        Debug.Log($"Added log entry to {logPath}");
+    }
+}
